f527cfc39c329587affdd1652465c76dfa642553
vivek8597 <vivekmehta8597@gmail.com>
2017-02-28 14:45:22 +0530

    Update and rename script.js to script1.js

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+// Object creation
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+company.ceo.favColor = "blue";
+
+console.log(company);
+console.log("Company CEO name is: " 
+  + company.ceo.firstName);
+
+console.log(company["name"]);
+var stockPropName = "stock of company";
+company[stockPropName] = 110;
+
+console.log("Stock price is: " + 
+  company[stockPropName]);
+
+
