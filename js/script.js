b163d23271a310536ceae5f7ccdc1218efae1261
vivek8597 <vivekmehta8597@gmail.com>
2017-03-21 15:20:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ document.addEventListener("DOMContentLoaded",
               else {
                 message += " scored58outof70inWebProgrammingand60outof70inOS ";
               }
-              message += " and uses ";
+              message += " and scored scored 58 out of 70 in Web Programming and 60 out of 70 in OS ";
               message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
+              message += "and 60 out of 70 in OS";
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
